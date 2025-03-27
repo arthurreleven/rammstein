891bc88cca85a7beec3a130f1cec2a2f4220c7be
Alex <alextavares1943@gmail.com>
2025-03-27 17:52:40 -0300

    empresa

--- src/Empresa/Dominio/Funcionario.java
+++ src/Empresa/Dominio/Funcionario.java
@@ -11,15 +11,25 @@ public class Funcionario {
     public void aumentoSalario() {
         if (salario >= 1500 && idade > 27) {
             salario = salario * 0.25;
-        }else {
-            System.out.println("Não teve aumento...");
+        } else {
+            System.out.println(nome + "Não teve aumento...");
         }
     }
 
-    public void  promoverFuncionario(){
+    public void promoverFuncionario() {
 
-        if (tempoServico >5 && idade>32){
-            System.out.println();
+        if (tempoServico > 5 && idade > 32) {
+            System.out.println(nome + "Gerente do setor...");
+        } else {
+            System.out.println(nome + "Não conseguiu a promoção...");
+        }
+    }
+
+    public void demitirFuncionario() {
+        if (idade > 55 & filhos < 2 && tempoServico < 10) {
+            System.out.println("Demitir " + nome);
+        } else {
+            System.out.println("Não demitir funcionario...");
         }
     }
 
