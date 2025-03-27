1656d863d958715b02bbd619eba20bf64c3c028a
Alex <alextavares1943@gmail.com>
2025-03-27 17:54:35 -0300

    empresa

--- src/Empresa/Test/EmpresaTest01.java
+++ src/Empresa/Test/EmpresaTest01.java
@@ -1,4 +1,25 @@
 package Empresa.Test;
 
+import java.util.Scanner;
+
 public class EmpresaTest01 {
+    public static void main(String[] args) {
+      Scanner entrada = new Scanner(System.in);
+      int opcao = validador();
+
+
+
+
+    }
+    public static int validador() {
+        Scanner entrada = new Scanner(System.in);
+        int opcao = entrada.nextInt();
+        if (opcao == 1 || opcao == 2) {
+            return opcao;
+        } else {
+            System.out.println("Opção invalida");
+        }
+        return opcao;
+
+    }
 }
