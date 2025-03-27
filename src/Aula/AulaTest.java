226f19b56c1433832d8d86cc4dae61325e2915a5
Alex <alextavares1943@gmail.com>
2025-03-27 10:20:41 -0300

    Aluno

---
+++ src/Aula/AulaTest.java
@@ -0,0 +1,35 @@
+package Aula;
+
+import java.util.Scanner;
+
+public class AulaTest {
+    public static void main(String[] args) {
+
+        Aluno aluno = new Aluno();
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Valor da nota1: ");
+        aluno.nota1 = entrada.nextInt();
+
+        System.out.print("Valor da nota2: ");
+        aluno.nota2 = entrada.nextInt();
+
+        System.out.print("Valor da nota3: ");
+        aluno.nota3 = entrada.nextInt();
+
+        System.out.print("Valor da nota41: ");
+        aluno.nota4 = entrada.nextInt();
+
+        System.out.print("Quantidade de presença: ");
+        aluno.presenca = entrada.nextInt();
+
+        System.out.print("Quantidade de faltas: ");
+        aluno.faltas = entrada.nextInt();
+
+        aluno.mediaNotaFianl();
+
+
+
+
+    }
+}
