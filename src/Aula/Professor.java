675be917aac5507ba9327bae01ebc5b9564462e9
Alex <alextavares1943@gmail.com>
2025-03-27 16:35:02 -0300

    Aluno

--- src/Aula/Professor.java
+++ src/Aula/Professor.java
@@ -1,4 +1,28 @@
 package Aula;
 
 public class Professor {
+
+    public double totalAlunos;
+    public double alunosAprovados;
+    public Aluno aluno;
+
+    public void promocao() {
+        double mediaAprovados = ((totalAlunos - alunosAprovados) / totalAlunos) * 100;
+        if (mediaAprovados >=75){
+            System.out.println("Professor promovido...");
+        }else {
+            System.out.println("Não promovido");
+        }
+    }
+
+    public void bonificacao(){
+        if (aluno.totalAulas >=30){
+            System.out.println("Direito a bonificação...");
+        }else {
+            System.out.println("Sem direito a bonificação...");
+        }
+    }
+
+
+
 }
