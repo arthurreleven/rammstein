228e7d362158863179cf55bfbb7ffe507d2f7447
Alex <alextavares1943@gmail.com>
2025-04-29 15:13:26 -0300

    Projeto pra semana que vem

--- src/TrabalhoParaSemanaQueVem/Dominio/Funcionario.java
+++ src/TrabalhoParaSemanaQueVem/Dominio/Funcionario.java
@@ -3,7 +3,12 @@ package TrabalhoParaSemanaQueVem.Dominio;
 public class Funcionario extends Pessoa {
 
     private double salario;
+    private String funcao;
 
+    public Funcionario(double salario, String funcao) {
+        this.salario = salario;
+        this.funcao = funcao;
+    }
 
     public void aumentoSalario() {
 
@@ -11,8 +16,27 @@ public class Funcionario extends Pessoa {
         salario = salario + (salario * 0.30);
         System.out.println("Salario com aumento:" + salario);
 
-
     }
 
+    public String mudarFuncao(String novaFuncao){
+        this.funcao = novaFuncao;
+        System.out.println(novaFuncao);
+        return novaFuncao;
+    }
 
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
 }
